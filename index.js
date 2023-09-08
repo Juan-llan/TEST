9fd51a1090438b5f5307bb5bd9ad9845bd8f6f52
Jllanca <juan.llanca37@gmail.com>
2023-09-08 18:04:21 -0300

    test

---
+++ index.js
@@ -0,0 +1,23 @@
+const lol= document.querySelector('#Formulario');
+
+Formulario.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const email = document.querySelector('#email');
+
+    const contrasena = document.querySelector('#contrasena');
+
+console.log(email,contrasena);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
