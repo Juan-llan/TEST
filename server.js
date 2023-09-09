43b3b35e7f34455403930d5d009822acd185ce89
benjaminespinoza <benjamintwo2002@gmail.com>
2023-09-09 16:17:11 -0300

    Ultimos cambios de benjamin, de las tareas de la semana

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const admin = require("firebase-admin");
+const credentials = require("./prueba-de-base-de-datos-912b8-firebase-adminsdk-4ydxs-79469cf326.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/signup', async (req, res) => {
+    console.log(req.body);
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+    }
+
+    try {
+        const userResponse = await admin.auth().createUser({
+            email: user.email,
+            password: user.password,
+            emailVerified: false,
+            disabled: false,
+        });
+        res.json(userResponse);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`);
+});
