d4a30220314462304846bb9c377d8f4de787564b
Patricio Quiroz R <themandosau116@gmail.com>
2023-09-26 00:20:21 -0300

    ewe

---
+++ Codigo/Login-Register/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum_scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+
+
+    <title>Login</title>
+</head>
+
+<body>
+
+    <div class="container-all">
+
+        <div class="ctn-form">
+            <h1 class="logo">Compass</h1>
+            <h1 class="tittle">Iniciar sesión</h1>
+
+            <form action="">
+
+                <label for="">Email</label>
+                <input type="text">
+                <span class="msg-error">Error uwu</span>
+
+                <label for="">Contraseña</label>
+                <input type="password">
+                <span class="msg-error">Error uwu</span>
+
+                <input type="submit" value="Iniciar">
+
+            </form>
+
+            <span class="text-footer">¿Aún no te has registrado?
+                <a href="register.php">Registrate</a>
+            </span>
+        </div>
+
+        <div class="ctn-text">
+
+            <div class="capa"></div>
+            <h1 class="tittle-description">Ve lo que viene</h1>
+            <p class="text-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam, nostrum. Aut provident consequuntur, perferendis molestiae repellendus ullam, beatae voluptas corrupti id ab possimus illum consequatur reiciendis velit veritatis similique temporibus.</p>
+
+        </div>
+
+    </div>
+</body>
+</html>
