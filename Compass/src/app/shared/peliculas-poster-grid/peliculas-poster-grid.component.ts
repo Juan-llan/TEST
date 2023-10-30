5f38a5a716782a359938e262e0e13158e9a5b5c7
benjaminespinoza <benjamintwo2002@gmail.com>
2023-10-30 17:27:48 -0300

    arreglos

---
+++ Compass/src/app/shared/peliculas-poster-grid/peliculas-poster-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-peliculas-poster-grid',
+  templateUrl: './peliculas-poster-grid.component.html',
+  styleUrls: ['./peliculas-poster-grid.component.css']
+})
+export class PeliculasPosterGridComponent {
+
+}
